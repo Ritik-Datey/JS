63de8b6552b0d1d11a34664acb3ff56c0d35d662
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-01-18 07:35:08 +0000

    new

---
+++ Basic/Datatype.js
@@ -0,0 +1,30 @@
+"use Strict";  // treat all JS code as a newer version...
+/*
+alert("ritik datey"); 
+here we are using a code editor so we can not use alert here
+*/
+
+// for documentation we recomended a mdn...
+
+let name= "Ritik" //String
+let age=25 //number
+let logedIn=true // boolean type
+
+/*
+number
+bigint
+String => ""
+boolean => true/false
+null => standalone value
+undefined
+symbol => to represent a unique value
+*/
+
+//object
+
+//to know the type od value we oser 'typeOf'
+
+console.log(typeof age) //number
+console.log(typeof "Ritik") // string
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
---
+++ Basic/Variables.js
@@ -0,0 +1,15 @@
+const name = "Ritik Datey"; //const represent a constanst value that can not be changed.
+let city="Nagpur"; // let is a another type of variables that we mostly used... and it is recomended to use 
+var ph_no = 7030417734; // var is a type of a varible and we can use it but not recomended to use it.
+
+email="ritikdatey2017@gmail.com" //without defining type we can also represent a variable...but this is bad programming practice.. 
+
+let state;//undefined value/variables
+let password=null; // null value
+/*
+prefered not to use a var because 
+it give a issue in a block scopeand functional code
+*/
+
+console.log("To print in a console we use this method")
+console.table([name, city, ph_no, email])//to print all data together....
