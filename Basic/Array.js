d66f8c1a7b1b04bb26e2c87f301e01ba2dca76e3
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-02-01 14:37:34 +0000

    new file

---
+++ Basic/Array.js
@@ -0,0 +1,13 @@
+const arr1=[1,2,3,4,5];
+// console.log(arr1);
+// arr1.push(6);
+// console.log(arr1);
+// arr1.pop();
+// console.log(arr1);
+// //arr1.pull(); not workable
+// console.log(arr1);
+
+const arr2=new Array(6,7,8,9);
+
+console.log(arr1.concat(arr2));
+
